802507b04ea8c27e97f8e89aaab6ee6fa0f159b4
mrtymn1980 <mrtymn1980@gmail.com>
2021-01-16 18:48:30 +0300

    v 0.1

--- index.js
+++ index.js
@@ -89,4 +89,14 @@ app.post("/sil", function(req, res){
         res.redirect("/");
     })
 });
+
+let port = process.env.PORT;
+if(port == "" || port == null){
+  port = 5000;
+}
+app.listen(port, function(){
+  console.log("port numarasi : " + port);
+});
+
+
 app.listen(5000);
